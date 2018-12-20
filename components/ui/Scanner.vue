8bc247d11edc18bb305d7487b300db2af9949f1c
qb_developer <dmitry@qbees.pro>
2018-12-20 18:58:57 +0200

    added Scanner component

---
+++ components/ui/Scanner.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h2>{{name}}</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                name: 'Dima'
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,31 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+    <div>
+        <scanner></scanner>
+        <div id="app">
+            <img src="./assets/logo.png">
+            <router-view/>
+        </div>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+    import Scanner from '../components/ui/Scanner.vue'
+
+    export default {
+        name: 'App',
+        components: {
+            'scanner': Scanner
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
